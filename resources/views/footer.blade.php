96b4252b305c3afa0f7c0ec88288b8a9b5e90941
unknown <anshulmankar84@gmail.com>
2021-03-22 14:53:41 +0530

    new

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="container">
+  <div class="panel panel-default">
+    <div class="panel-body">Panel Content</div>
+    <div class="panel-footer">Panel Footer</div>
+  </div>
+</div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,29 @@
+
+ 
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+ 
+  <a class="navbar-brand" href="#">Logo</a>
+
+
+  <ul class="navbar-nav active">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Orders</a>
+    </li>
+  
+  <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+  </form>
+</ul>
+
+<div class="container text-right">
+    <p class="d-inline">Order Carded</p>
+    <button type="button" class="btn btn-primary d-inline">Card(0)</button>
+</div>
+      
+</nav>
+ 
